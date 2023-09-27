eea8bd81e360c5c91e4d437b0e1a4895663ed1b0
Muneeb Ul Haq Khan <73172919+12muneeb@users.noreply.github.com>
2023-09-27 18:38:36 +0500

    todo

--- src/redux/rootReducer.js
+++ src/redux/rootReducer.js
@@ -3,10 +3,12 @@ import {reducer} from './reducers';
 import {authReducer} from './reducers/authReducer';
 import {userReducer} from './reducers/userReducer';
 import {postReducer} from './reducers/postReducer';
+import {AddReducer} from './reducers/AddTodoReducer';
 
 export default combineReducers({
   auth: authReducer,
   user: userReducer,
   post: postReducer,
+  todo: AddReducer,
   product: reducer,
 });
